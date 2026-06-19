9f29705d0d4751f75cca339f8f0987b15917d42b
Tech Support <tech@smsidea.co.in>
2026-06-19 17:41:32 +0530

    testdataw

--- WebApplication8/Controllers/EmployeesController.cs
+++ WebApplication8/Controllers/EmployeesController.cs
@@ -52,7 +52,6 @@ namespace WebApplication8.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Employee>> GetById(int id)
         {
-
             var employee = await _db.Employees.FindAsync(id);
             return employee is null ? NotFound() : employee;
         }
